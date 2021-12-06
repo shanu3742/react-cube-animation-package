4075f331e4973ed7c6484e2b6d2094b86624b0ee
Kumar Shanu <shanuK@aarth.io>
2021-12-06 17:11:57 +0530

    external css deleted

--- src/index.jsx
+++ src/index.jsx
@@ -1,82 +1,173 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
+const Cube = ({top_Color,box_plate_linear_gradient_first_color,box_plate_linear_gradient_Second_color,box_Shadow_Color,size}) => {
 
-const Outlined = styled.button`
-    font-size: 16px;
-    padding: 15px;
-    border: 1px solid #e0115f;
-    outline: none;
-    box-shadow: none;
-    background-color: white;
-    color: #e0115f;
-    text-decoration: none;
-    transition: border 0.25s, background 0.25s, color 0.25s;
-    &:hover {
-        cursor: pointer;
-        color: #990c41;
-        border-color: #990c41;
-        background-color: #dddddd;
+   
+const halfOfSize=size/2
+
+console.log(halfOfSize)
+   
+    const span0={
+     
+        position: `absolute`,
+        top: `0`,
+        left: 0,
+        width: `100%`,
+        height: `100%`,
+        backgroundImage: `linear-gradient(${box_plate_linear_gradient_first_color},${box_plate_linear_gradient_Second_color} )`,
+        transform: `rotateY(0deg) translateZ(${halfOfSize}rem)`
     }
-`;
-
-const Contained = styled.button`
-    font-size: 16px;
-    padding: 15px;
-    border: 0;
-    outline: none;
-    box-shadow: none;
-    background-color: #e0115f;
-    color: white;
-    text-decoration: none;
-    transition: border 0.25s, background 0.25s, color 0.25s;
-    &:hover {
-        cursor: pointer;
-        background-color: #990c41;
-    }
-`;
-
-
-const Text = styled.button`
-    font-size: 16px;
-    padding: 15px;
-    border: 0;
-    outline: none;
-    box-shadow: none;
-    background-color: transparent;
-    color: #e0115f;
-    text-decoration: none;
-    &:hover {
-        cursor: pointer;
-        text-decoration: underline;
-    }
-`;
-
-const FancyButton = ({label, type, variant, onClick, ...props}) => {
-
-    let ButtonStyled = Outlined;
-    if(variant === 'contained'){
-        ButtonStyled = Contained;
-    }
-    else 
-    if(variant === 'text'){
-        ButtonStyled = Text;
+    const span1={
+     
+        position: `absolute`,
+        top: `0`,
+        left: 0,
+        width: `100%`,
+        height: `100%`,
+        backgroundImage: `linear-gradient(${box_plate_linear_gradient_first_color},${box_plate_linear_gradient_Second_color} )`,
+        transform: `rotateY(90deg) translateZ(${halfOfSize}rem)`
     }
 
-    return <ButtonStyled {...props} type={type} onClick={onClick}>{label}</ButtonStyled>;
+    const span2={
+        position: `absolute`,
+        top: `0`,
+        left: 0,
+        width: `100%`,
+        height: `100%`,
+
+        backgroundImage: `linear-gradient(${box_plate_linear_gradient_first_color},${box_plate_linear_gradient_Second_color} )`,
+        transform: `rotateY(180deg) translateZ(${halfOfSize}rem)`
+    }
+    const span3={
+         position: `absolute`,
+        top: `0`,
+        left: 0,
+        width: `100%`,
+        height: `100%`,
+
+        backgroundImage: `linear-gradient(${box_plate_linear_gradient_first_color},${box_plate_linear_gradient_Second_color} )`,
+        transform: `rotateY(270deg) translateZ(${halfOfSize}rem)`
+    }
+   
+    const top_Box_Color =top_Color
+    const shadow_color=box_Shadow_Color
+
+    const araayofShadowColor = shadow_color.split(',')
+
+    const changeNumber = araayofShadowColor[2]
+
+    const anotherchange=changeNumber.split(')') 
+
+    const changeNumberatZero = araayofShadowColor[0]
+
+    const anothercolorchange=changeNumberatZero.split('(') 
+
+const FirstFixedColor =anothercolorchange[1]
+    
+
+    const secondFixedColor = araayofShadowColor[1]
+
+    console.log(secondFixedColor)
+    
+
+    const changableColorCode =anotherchange[0]
+
+    const shadow_first_light_color=`rgb(${FirstFixedColor}, ${secondFixedColor},${changableColorCode+2})`
+    const shadow_second_light_color=`rgb(${FirstFixedColor}, ${secondFixedColor},${changableColorCode+4})`
+    const shadow_third_light_color=`rgb(${FirstFixedColor}, ${secondFixedColor},${changableColorCode+6})`
+    const shadow_fourth_light_color=`rgb(${FirstFixedColor}, ${secondFixedColor},${changableColorCode+8})`
+
+    const actualSize = parseInt(size)/3
+
+   
+   
+
+    const BlurDiv = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: ${size}rem;
+    height: ${size}rem;
+     background: ${top_Box_Color }; 
+    transform: rotateX(90deg) translateZ(${halfOfSize}rem);
+    transform-style: preserve-3d;
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: ${size}rem;
+        height: ${size}rem;
+        background-color: ${box_plate_linear_gradient_Second_color,box_Shadow_Color };
+        transform: translateZ(-${size}rem);
+        filter: blur(20px);
+        box-shadow: 0 0 ${actualSize}rem ${shadow_color}, 0 0 ${actualSize+80}rem ${shadow_first_light_color},
+        0 0 ${actualSize+180}rem ${shadow_second_light_color}, 0 0 ${actualSize+280}rem ${shadow_third_light_color},
+        0 0 ${actualSize+380}rem ${shadow_fourth_light_color};
+      }
+      `
+
+    //   <div className ='top' style={top}></div>
+    const animate = keyframes`
+    0% {
+        transform: rotateX(-30deg) rotateY(0deg);
+      }
+      100% {
+        transform: rotateX(-30deg) rotateY(360deg);
+      }
+  `
+ 
+const MainContainer =styled.div`
+position: relative;
+transform-style: preserve-3d;
+transform: rotateX(-30deg);
+animation:${animate} 4s linear infinite;
+width:${size}rem;
+height:${size}rem
+`
+
+// .cube div {
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     transform-style: preserve-3d;
+//   }
+const Platediv = styled.div`
+position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+`
+const BottomDiv = styled.div`
+position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+`
+    return (
+        <>
+        <MainContainer >
+        <BlurDiv></BlurDiv>
+       
+            <Platediv>
+                    <span id='span0' style={span0}></span>
+                    <span id='span1' style={span1}></span>
+                    <span id='span2' style={span2}></span>
+                    <span id='span3' style={span3}></span>
+            </Platediv>
+            
+        </MainContainer>
+        
+      
+       </>
+    )
 }
 
-FancyButton.defaultProps = {
-    type: 'button',
-    variant: 'outlined'
-}
-
-FancyButton.propTypes = {
-    label: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(['button', 'submit', 'reset']),
-    variant: PropTypes.oneOf(['outlined', 'contained', 'text']),
-    onClick: PropTypes.func.isRequired
-}
-
-export {FancyButton};
+export default Cube
